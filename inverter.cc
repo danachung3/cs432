05938dab6d71e143ce1dc4436a954c4cf19e0753
danachung3 <danachung3@gmail.com>
2019-02-09 18:18:25 -0500

    first commit

---
+++ inverter.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <sstream>
+
+
+typedef std::map<std::string, std::string> invertedIndex;
+
+void countWords(std::string word, invertedIndex& words, std::string i){
+  words[word] = words[word] + i;
+}
+
+int main() {
+
+  std::ifstream file ("inputs.txt");
+  if (!file.is_open()) return 0;
+
+  invertedIndex w;
+  std::string line;
+  int i = 0;
+
+  while (getline(file, line)){
+
+    std::ostringstream ss;
+    ss << " " << "i";
+
+    std::ifstream files (line.c_str());
+    if (!files.is_open()) return 0;
+    std::string word;
+    while (files >>word)
+      {
+        countWords(word, w, ss.str());
+      }
+    ++i;
+  }
+
+
+  for (invertedIndex::iterator p = w.begin();
+       p!= w.end(); ++p) {
+    std::cout << p->first << ":"
+              << p->second << "\n";
+  }
+}
