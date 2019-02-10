6a82f1fc4a772075c0a16a273801e88eda0d9ffc
James Lovette <21jil1@lohani.cs.williams.edu>
2019-02-10 16:19:02 -0500

    almost there

--- inverter.cc
+++ inverter.cc
@@ -19,6 +19,35 @@ void addIndex(std::string word, invertedIndex& words, std::string i){
   words[word] = words[word] + i;
 }
 
+bool isLetter(char c) {
+  if(int(c) < 65 || int(c) > 122 || (int(c) > 90 && int(c) < 97)) {
+    return false;
+  }
+  return true;
+}
+	  
+
+void parseWord(std::string word, invertedIndex& w, std::string i){
+  if(word.length() == 0 || int(word.at(0)) == 32) {
+    return;
+  }
+
+  for(int j = 0; j < word.length(); ++j) {
+    if(!isLetter(word.at(j))) {
+      std::string str1 = word.substr(0, j);
+      std::string str2 = word.substr(j+1);
+      //std::cout << str1 + "\n";
+      //std::cout << str2 + "\n";
+      parseWord(str1, w, i);
+      parseWord(str2, w, i);
+      return;
+    }
+  }
+  addIndex(word, w, i);
+}
+  
+    
+
 int main(int argc, char** argv) {
   std::string filename = argv[1];
   std::ifstream file (filename.c_str());
@@ -37,11 +66,14 @@ int main(int argc, char** argv) {
     std::string word;
     while (files >>word)
       {
-	for(int j = 0; j < word.length(); ++j){
-	  if(int(word.at(j)) < 65 || int(word.at(j)) > 122 || (int(word.at(j)) < 90 && int(word.at(j)) < 97))
+	parseWord(word, w, ss.str());
+	/**for(int j = 0; j < word.length(); ++j){
+	  if(int(word.at(j)) < 65 || int(word.at(j)) > 122 || (int(word.at(j)) < 90 && int(word.at(j)) < 97)) {
 	      word.erase(j, 1);
+	      --j;
+	  }	  
 	}
-	  addIndex(word, w, ss.str());
+	addIndex(word, w, ss.str());*/
       }
 	++i;
   }
