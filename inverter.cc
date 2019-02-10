abf29b02a161726802e14bc75e47bb43cdfbf1bc
James Lovette <21jil1@lohani.cs.williams.edu>
2019-02-10 16:38:07 -0500

    nonopenable

--- inverter.cc
+++ inverter.cc
@@ -62,11 +62,11 @@ int main(int argc, char** argv) {
     std::ostringstream ss;
     ss << " " << i;
     std::ifstream files (line.c_str());
-    if (!files.is_open()) return 0;
-    std::string word;
-    while (files >>word)
-      {
-	parseWord(word, w, ss.str());
+    if (files.is_open()) {
+      std::string word;
+      while (files >>word)
+	{
+	  parseWord(word, w, ss.str());
 	/**for(int j = 0; j < word.length(); ++j){
 	  if(int(word.at(j)) < 65 || int(word.at(j)) > 122 || (int(word.at(j)) < 90 && int(word.at(j)) < 97)) {
 	      word.erase(j, 1);
@@ -74,10 +74,10 @@ int main(int argc, char** argv) {
 	  }	  
 	}
 	addIndex(word, w, ss.str());*/
-      }
-	++i;
+	}
+      ++i;
+    }
   }
-  
 
   for (invertedIndex::iterator p = w.begin();
        p!= w.end(); ++p) {
