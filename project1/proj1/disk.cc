b19b2f1a819ac85475d36b1ebc25ece8cec1c635
Dana H. Chung <19dhc1@lohani.cs.williams.edu>
2019-02-26 16:28:10 -0500

    progress

--- project1/proj1/disk.cc
+++ project1/proj1/disk.cc
@@ -6,36 +6,38 @@
 using namespace std;
 
 vector<tuple<int,int>> queue;
-int livethreads;
+int livethreads = 0;
 int lock = 0;
 int hasRequest = 1;
 int hasRoom = 2;
 int canTake = 3;
+int max_disk_queue = 0;
+vector<string> filenames;
 
-bool queueFull() {return 1;}
-bool queueEmpty() {return 1;}
-tuple<int,int> queuePop(int disk) {return queue[0];}
+bool queueFull() {
+  return  queue.size() ==  max_disk_queue; 
+}
+bool queueEmpty() {
+  return queue.size() == 0; 
+  
+}
 
-void consumer() {
-  int disk = 0;
-  //While there are still requests to handle
-  while(livethreads > 0 && !queueEmpty()) {
-    thread_lock(lock);
-    //Wait until the queue is as full as possible so can take most efficient disk track
-    while(!queueFull() && livethreads) {
-      thread_wait(lock, canTake);
-    }
-    //Gets closest request to current disk
-    tuple<int,int> request = queuePop(disk);
-    disk = get<1>(request);
-    cout << "service requester " << get<0>(request) << " track " << get<1>(request) << endl;
-    //Calls for another producer
-    thread_signal(lock, hasRoom);
-    thread_unlock(lock);
+tuple<int,int> queuePop(int disk) {
+  int index = 0;
+  int min = abs(disk - get<1>(queue[0]));
+  for(int i = 0; i < queue.size(); i++){
+    if (min > abs(disk - get<1>(queue[i]))){
+	min = abs(disk - get<1>(queue[i]));
+	index = i;
+      }
   }
+  tuple<int,int> minimum  = queue[index]; 
+  queue.erase(queue.begin()+index); 
+  return minimum; 
 }
 
 void producer(void* arg) {
+  cout << arg; 
   int fileNumber = 0;
   string line;
   string &filename = *(static_cast<string*>(arg));
@@ -55,23 +57,48 @@ void producer(void* arg) {
   }
 }
 
+
+
+void consumer() {
+  int disk = 0;
+  for(int i = 0; i < filenames.size(); i++){
+    thread_create((thread_startfunc_t) producer, (void *) static_cast<void*>(&filenames[i]));
+  }
+  //While there are still requests to handle
+  while(livethreads > 0 || !queueEmpty()) {
+    thread_lock(lock);
+
+    //Wait until the queue is as full as possible so can take most efficient disk track
+    while(!queueFull() && livethreads > 0 ) {
+      thread_wait(lock, canTake);
+    }
+    //Gets closest request to current disk
+    tuple<int,int> request = queuePop(disk);
+    disk = get<1>(request);
+    cout << "service requester " << get<0>(request) << " track " << get<1>(request) << endl;
+    //Calls for another producer
+    thread_signal(lock, hasRoom);
+    thread_unlock(lock);
+  }
+}
+
+
 int main(int argc, char** argv) {
-  int max_disk_queue = stoi(argv[1], nullptr, 0); //maximum number of requests the disk can hold
+  max_disk_queue = stoi(argv[1], nullptr, 0); //maximum number of requests the disk can hold
   int requester_thread_count = argc - 2; //number of threads making disk requests
   //void* a;
   
   for (int i = 2; i < argc; ++i) {//for every input file...
-    string filename = argv[i];
+    auto iter = filenames.insert(filenames.end(), argv[i]);
+
+    //  string filename = argv[i];
   //ifstream file (filename.c_str());
   //string line;
   //if (!file.is_open()) return 0; //file cannot be opened
-    cout << "creating " << filename << "\n";
-    thread_create((thread_startfunc_t) producer, (void *) static_cast<void*>(&filename));//create thread??
+  //  cout << "creating " << filename << "\n";
+  //  thread_create((thread_startfunc_t) producer, (void *) static_cast<void*>(&filename));//create thread??
       //while (getline (file, line)){ //for each line in the input file...
-      // int track_number = line; // get track number of request
-
-
-
+      // int track_number = line;
     }
   thread_libinit((thread_startfunc_t ) consumer, (void *) 5);
 
