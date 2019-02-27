d42cb9754eedb7fe7a86eee859da1422f3369af7
James Lovette <21jil1@lohani.cs.williams.edu>
2019-02-27 13:08:10 -0500

    almost there

--- project1/proj1/disk.cc
+++ project1/proj1/disk.cc
@@ -5,22 +5,28 @@
 #include <tuple>
 using namespace std;
 
-vector<tuple<int,int>> queue;
+
 int livethreads = 0;
-int lock = 0;
-int hasRequest = 1;
-int hasRoom = 2;
-int canTake = 3;
+int requests = 0;
+int lock = 100;
+int hasRequest = 110;
+int hasRoom = 120;
+int canTake = 130;
+int live = 140;
 int max_disk_queue = 0;
-vector<string> filenames;
+vector<tuple<int,string>> filenames;
+vector<tuple<int,int>> queue;
 
 bool queueFull() {
-  return  queue.size() ==  max_disk_queue; 
+  return queue.size() == max_disk_queue; 
 }
 bool queueEmpty() {
-  return queue.size() == 0; 
-  
+  return queue.size() == 0;   
 }
+bool needsToWait() {
+  return !queueFull();
+}
+
 
 tuple<int,int> queuePop(int disk) {
   int index = 0;
@@ -37,48 +43,67 @@ tuple<int,int> queuePop(int disk) {
 }
 
 void producer(void* arg) {
-  cout << arg; 
-  int fileNumber = 0;
+  thread_lock(live);
+  livethreads++;
+  thread_unlock(live);
   string line;
-  string &filename = *(static_cast<string*>(arg));
-  ifstream file (filename);
-  cout << "Processing " << filename;
+  tuple<int,string>* fileInfo = (tuple<int,string>*) arg;
+  int fileNumber = get<0>(*fileInfo);
+  string fileName = get<1>(*fileInfo);
+  ifstream file (fileName);
+  //  cout << "Processing " << fileName << "\n";
   while(getline (file, line)) {//file a has more disk requests
     thread_lock(lock);
+    requests++;
+    //cout << filename << " aquired lock \n";
     while(queueFull()) {
       thread_wait(lock, hasRoom);
     }
+    //cout << filename << " is done waiting \n";
     int track = stoi(line, nullptr, 0); //String to int conversion
     tuple<int,int> request = make_tuple(fileNumber,track);
     auto iter = queue.insert (queue.begin(), request);
     cout << "requester " << fileNumber << " track " << track << endl;
     thread_signal(lock, hasRequest);
+    //cout << "Signaling hasRequest\n";
+    //Waiting until specific request is serviced
+    thread_wait(lock, fileNumber);
+    requests--;
     thread_unlock(lock);
   }
+  thread_lock(live);
+  livethreads--;
+  thread_unlock(live);
 }
 
 
 
 void consumer() {
   int disk = 0;
-  for(int i = 0; i < filenames.size(); i++){
-    thread_create((thread_startfunc_t) producer, (void *) static_cast<void*>(&filenames[i]));
-  }
-  //While there are still requests to handle
-  while(livethreads > 0 || !queueEmpty()) {
+  while(1){
     thread_lock(lock);
-
-    //Wait until the queue is as full as possible so can take most efficient disk track
-    while(!queueFull() && livethreads > 0 ) {
-      thread_wait(lock, canTake);
+    //Need to wait until queue is full to get optimal disk track pick
+    while(needsToWait()){//&& livethreads > 0 ) {
+      thread_wait(lock, hasRequest);
     }
-    //Gets closest request to current disk
+   
+    //Processes disk request
     tuple<int,int> request = queuePop(disk);
     disk = get<1>(request);
     cout << "service requester " << get<0>(request) << " track " << get<1>(request) << endl;
-    //Calls for another producer
+
+    //Alerts specific producer that the request has been serviced
     thread_signal(lock, hasRoom);
+    thread_signal(lock, get<0>(request));
     thread_unlock(lock);
+  }
+}
+
+void beginThreads() {
+  thread_create((thread_startfunc_t ) consumer, (void *) 5);
+  for(int i = 0; i < filenames.size(); i++){
+    thread_create((thread_startfunc_t) producer, (void *) static_cast<void*>(&filenames[i]));
+    thread_yield();
   }
 }
 
@@ -86,10 +111,9 @@ void consumer() {
 int main(int argc, char** argv) {
   max_disk_queue = stoi(argv[1], nullptr, 0); //maximum number of requests the disk can hold
   int requester_thread_count = argc - 2; //number of threads making disk requests
-  //void* a;
-  
   for (int i = 2; i < argc; ++i) {//for every input file...
-    auto iter = filenames.insert(filenames.end(), argv[i]);
+    tuple<int,string> fileInfo = make_tuple(i - 2, argv[i]);
+    auto iter = filenames.insert(filenames.end(), fileInfo);
 
     //  string filename = argv[i];
   //ifstream file (filename.c_str());
@@ -100,6 +124,6 @@ int main(int argc, char** argv) {
       //while (getline (file, line)){ //for each line in the input file...
       // int track_number = line;
     }
-  thread_libinit((thread_startfunc_t ) consumer, (void *) 5);
+  thread_libinit((thread_startfunc_t ) beginThreads, (void *) 5);
 
 }
