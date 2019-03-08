60cf0c87dd6d48c5d6b88c9245f407fd94567ea6
danachung3 <danachung3@gmail.com>
2019-03-08 09:45:18 -0500

    more tests

--- project1/proj1/initTest1.cc
+++ project1/proj1/initTest1.cc
@@ -1,9 +1,13 @@
-#include <iostream> 
+#include <iostream>
 #include <fstream>
-#include "thread.h" 
-using namespace std; 
+#include "thread.h"
+using namespace std;
 
 int lock = 0;
+
+
+// This test tests thread_create before running thread_libinit and calling thread_libinit twice 
+
 
 void thread1(void* arg) {
   int* num = (int*) arg;
@@ -16,29 +20,12 @@ void thread1(void* arg) {
 
 
 void startHelper() {
-  thread_libinit((thread_startfunc_t ) startHelper, (void *) 5); 
-  cout << thread_libinit((thread_startfunc_t) startHelper, (void *) 5); 
-  cout << "\nThis should fail again 2\n"; 
-  
+  thread_libinit((thread_startfunc_t ) startHelper, (void *) 5);
 }
-
-void secondHelper() {
-  thread_create((thread_startfunc_t ) thread1, (void *) 1);
-}
-
 
 int main(int argc, char** argv) {
-  //  thread_libinit((thread_startfunc_t ) startHelper, (void *) 5);
 
-  cout << "\ntesting\n"; 
-  cout << thread_create((thread_startfunc_t ) thread1, (void *) 1); 
-  //  return thread_create((thread_startfunc_t ) thread1, (void *) 1);
-  cout << "\nThis should fail\n";
-
+  thread_create((thread_startfunc_t ) thread1, (void *) 1);
   thread_libinit((thread_startfunc_t ) startHelper, (void *) 5);
-  // return 1; 
+  // return 1;
 }
-
-
-
-
