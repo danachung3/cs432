7efd9416cb90dcaace7ad639a2c76a8a213048d0
Dana H. Chung <19dhc1@dexter.cs.williams.edu>
2019-04-09 19:37:29 -0400

    progress

--- proj2/pager.cc
+++ proj2/pager.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include "vm_pager.h"
+//#include "vm_pager.h"
 #include <vector>
 #include <tuple>
 #include <map>
 #include <stack>
+#include "vm_pager.h"
+
 using namespace std;
 
 struct vpage_t {
@@ -27,31 +29,58 @@ stack<int> physicalMem;
 stack<int> disk; 
 process_t currentProc;
 
-
 extern void vm_init(unsigned int memory_pages, unsigned int disk_blocks) {
-  //What do we actually need to do here?
+  for(int i = 0; i < memory_pages; i++){
+    physicalMem.push(i); 
+  }
+  for(int i = 0; i < disk_blocks; i++){
+    disk.push(i); 
+  }
   
-  //physicalMem = new stack<int>(memory_pages);
-  //disk = new int [disk_blocks];
   // map<pid_t, process_t> processes;
+
+  struct process_t nil = {NULL, NULL, NULL}; 
+  currentProc = nil; 
 }
 
 extern void vm_create(pid_t pid) {
   page_table_t pt;
   vector<vpage_t> v;
   struct process_t newProcess = {pid, pt, v};
-  if(current == NULL) {
-    current = newProcess;
+  if(currentProc.processID == NULL) {
+     currentProc = newProcess;
     //Set page_table_base_register to be pt
+     page_table_base_register = &pt; 
   }
   processes.insert(pair<pid_t, process_t>(pid, newProcess));
 }
 
+extern void * vm_extend(){
+  //Make anothe virtual page
+
+  //if there is enough available disk blocks to hold all virtual pages 
+
+  struct vpage_t pte = {0, 0, 0, 0, 0, 0};
+  currentProc.vPages.insert(currentProc.vPages.end(), pte); 
+  
+
+  //else return NULL
+
+};
+
+extern int vm_fault(void *addr, bool write_flag){
 
 
 
-extern void vm_switch(pid_t pid);
-extern int vm_fault(void *addr, bool write_flag);
-extern void vm_destroy();
-extern void * vm_extend();
-extern int vm_syslog(void *message, unsigned int len);
+};
+
+extern void vm_switch(pid_t pid){};
+
+extern void vm_destroy(){
+
+
+
+};
+
+
+extern int vm_syslog(void *message, unsigned int len){};
