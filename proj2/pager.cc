251c6ed2f0a367ef530b25aa7de9a54968027809
Dana H. Chung <19dhc1@dexter.cs.williams.edu>
2019-04-15 11:39:03 -0400

    good push

--- proj2/pager.cc
+++ proj2/pager.cc
@@ -18,12 +18,14 @@ struct vpage_t {
   int disk_block;
   bool read;
   bool write;
+  bool reference;
 };
 
 struct process_t {
   pid_t processID;
   page_table_t pageTable;
   vector<vpage_t*> vPages;
+  vector<vpage_t*> clock;
 };
 
 map<pid_t, process_t*> processes;
@@ -38,8 +40,6 @@ extern void vm_init(unsigned int memory_pages, unsigned int disk_blocks) {
   for(int i = 0; i < disk_blocks; i++){
     disk.push(i);
   }
-//  struct process_t nil = {NULL, NULL, NULL};
-//  currentProc = nil;
 }
 
 extern void vm_create(pid_t pid) {
@@ -47,7 +47,7 @@ extern void vm_create(pid_t pid) {
   newProcess->processID = pid;
 
   for(int i = 0; i < VM_ARENA_SIZE/VM_PAGESIZE ; i++){
-    newProcess->pageTable.ptes[i].ppage = 100000;
+    newProcess->pageTable.ptes[i].ppage = 10000;
     newProcess->pageTable.ptes[i].read_enable = 0;
     newProcess->pageTable.ptes[i].write_enable = 0;
   }
@@ -69,6 +69,7 @@ extern void * vm_extend(){
   pte->disk_block = diskLoc;
   pte->read = 0;
   pte->write = 0;
+  pte->reference = 0;
 
   currentProc.vPages.insert((currentProc.vPages.end()), pte);
 
@@ -78,19 +79,12 @@ extern void * vm_extend(){
 
 extern int vm_fault(void *addr, bool write_flag){
   //If the physical memory process has not been set
-  //define pm_physmem
-
-  //assuming addr is the address of the vpage_t
-  //get index to find page_table_t entry, not sure if thats right?
-  //  int index = (addr - VM_ARENA_BASEADDR) / VM_PAGESIZE;
   unsigned int index = (unsigned int)((unsigned long) addr - (unsigned long)VM_ARENA_BASEADDR) / VM_PAGESIZE;
-  // unsigned int index = (unsigned int)((unsigned long) addr / VM_PAGESIZE);
 
-
-  //int index = ((int)addr - (int)VM_ARENA_BASEADDR) / (int)VM_PAGESIZE;
   page_table_entry_t page_table_entry = currentProc.pageTable.ptes[index]; 
   vpage_t* current_vpage = currentProc.vPages[index];
   int i = 0;
+
   //if address to invalid page
   /**if (i){
     return -1;
@@ -98,22 +92,46 @@ extern int vm_fault(void *addr, bool write_flag){
 
 
   //if virtual page does not have a physical page
-  if (page_table_entry.ppage == 100000){
-    int ppage = physicalMem.top();
-    physicalMem.pop();
+  if (page_table_entry.ppage == 10000){
+
     
-    memset((char*) pm_physmem + ppage * VM_PAGESIZE, 0, VM_PAGESIZE);
+    
+    if (physicalMem.empty()){
+      //no free space in physical mem, need to evict
 
-    currentProc.pageTable.ptes[index].ppage = ppage;
-    currentProc.vPages[index]->resident = 1;
-    currentProc.vPages[index]->zero = 0;
+      while (currentProc.clock.at(0)->reference == 1){
+	currentProc.clock.at(0)->reference = 0;
+	currentProc.clock.insert((currentProc.clock.end()), currentProc.clock.at(0));
+	currentProc.clock.erase(currentProc.clock.begin());
+      }
+   
+	//reference bit is zero, evict it
+      if (currentProc.clock.at(0)->dirty){
+	  //write to disk, access diskBlock int, pageTable index of virtual page  
+	  //disk_write(currentProc.clock.at(0)->diskBlock, currentProc.pageTable[
+      }
+      else{
+	  currentProc.clock.at(0)->resident = 0;
+	  //need to find index... and set pppage to -1 or 10000 
+	  currentProc.pageTable.ptes[index];
+	  
+	  
+      }
 
+      
+    }
+    else {
+      //free space in physical mem
+      int ppage = physicalMem.top();
+      physicalMem.pop();
+    
+      memset((char*) pm_physmem + ppage * VM_PAGESIZE, 0, VM_PAGESIZE);
 
-    //    page_table_entry.ppage = ppage;
-
-    //    current_vpage->resident = 1; 
-    //current_vpage->zero = 0; 
-
+      currentProc.pageTable.ptes[index].ppage = ppage;
+      currentProc.vPages[index]->resident = 1;
+      currentProc.vPages[index]->zero = 0;
+      currentProc.clock.insert((currentProc.clock.end()), currentProc.vPages[index]);
+    }
   }
 
 
@@ -121,13 +139,10 @@ extern int vm_fault(void *addr, bool write_flag){
   if (!write_flag){
 
     if (page_table_entry.read_enable == 0){
-      //do stuff
       
       currentProc.pageTable.ptes[index].read_enable = 1;
       currentProc.vPages[index]->read = 1;
-
-      //      page_table_entry.read_enable = 1;
-      //current_vpage->read = 1;
+      currentProc.vPages[index]->reference = 1;
 
       return 0; 
     }
@@ -145,13 +160,8 @@ extern int vm_fault(void *addr, bool write_flag){
       currentProc.vPages[index]->read = 1;
       currentProc.vPages[index]->write = 1;
       currentProc.vPages[index]->dirty = 1;
-      /**      
-      page_table_entry.read_enable = 1;
-      page_table_entry.write_enable = 1;
-      current_vpage->read = 1;
-      current_vpage->write = 1;
-      current_vpage->dirty = 1;
-      */
+      currentProc.vPages[index]->reference = 1;
+ 
       return 0; 
       
     }
