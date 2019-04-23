083dd81dc39e09afb29aa2020c97277959832648
James Lovette <21jil1@lohani.cs.williams.edu>
2019-04-23 19:04:53 -0400

    progress

--- proj2/pager.cc
+++ proj2/pager.cc
@@ -9,7 +9,7 @@
 #include <queue>
 #include "vm_pager.h"
 #include <cstdint>
-
+#include <string>
 using namespace std;
 
 struct vpage_t {
@@ -27,7 +27,6 @@ struct process_t {
   page_table_t pageTable;
   vector<vpage_t*> vPages;
   queue<tuple<int,vpage_t*>> clock;
-  //  vector<vpage_t*> clock;
 };
 
 map<pid_t, process_t*> processes;
@@ -82,70 +81,66 @@ extern void * vm_extend(){
 extern int vm_fault(void *addr, bool write_flag){
   //If the physical memory process has not been set
   unsigned int index = (unsigned int)((unsigned long) addr - (unsigned long)VM_ARENA_BASEADDR) / VM_PAGESIZE;
-
   page_table_entry_t page_table_entry = currentProc.pageTable.ptes[index]; 
   vpage_t* current_vpage = currentProc.vPages[index];
   int i = 0;
 
   //if virtual page does not have a physical page
   if (page_table_entry.ppage == 10000){    
+
+    //If no physical memory available, must run clock algo
     if (physicalMem.empty()){
       //no free space in physical mem, need to evict
 
+      //Loop through clock struct until reference bit of first element is 0
       while (get<1>(currentProc.clock.front())->reference == 1){
 	get<1>(currentProc.clock.front())->reference = 0;
 	currentProc.clock.push(currentProc.clock.front());
 	currentProc.clock.pop();
       }
-   
-	//reference bit is zero, evict it
+
+      //If its dirty, write to disk
       if (get<1>(currentProc.clock.front())->dirty){
 	disk_write(get<1>(currentProc.clock.front())->disk_block, get<0>(currentProc.clock.front()));
-	//write to disk, access diskBlock int, pageTable index of virtual page  
-	//disk_write(currentProc.clock.at(0)->diskBlock, currentProc.pageTable[
+	get<1>(currentProc.clock.front())->dirty = 0;
+	get<1>(currentProc.clock.front())->read = 0;
+	get<1>(currentProc.clock.front())->write = 0;
+	get<1>(currentProc.clock.front())->resident = 0;
+	get<1>(currentProc.clock.front())->reference = 0;
       }
-	//else{
-	//get<1>(currentProc.clock.front())->reference = 0;
-	//	  currentProc.clock.at(0)->resident = 0;
-	  //need to find index... and set pppage to -1 or 10000 
-	  //currentProc.pageTable.ptes[index];
-      int evict = get<0>(currentProc.clock.front());
-      
-      tuple<int, vpage_t*> temp = make_tuple(evict, currentProc.vPages[index]);
-      currentProc.clock.push(temp);
 
-      get<1>(currentProc.clock.front())->resident = 0;
-      currentProc.pageTable.ptes[index].ppage = evict;
-      //      currentProc.pageTable.ptes[evict].ppage = 10000;
-      currentProc.clock.pop();	  
-      
+      //Eviction stuff, check if this is ok
+      int evictIndex = get<0>(currentProc.clock.front());
+      physicalMem.push(currentProc.pageTable.ptes[evictIndex].ppage);
+      currentProc.pageTable.ptes[evictIndex].ppage = 10000;
+      currentProc.pageTable.ptes[evictIndex].read_enable = 0;
+      currentProc.pageTable.ptes[evictIndex].write_enable = 0;
+            
+    }
+    //Grabbing free memory
+    int ppage = physicalMem.top();
+    physicalMem.pop();
 
-      //}
-
-      
+    currentProc.pageTable.ptes[index].ppage = ppage;
+    currentProc.vPages[index]->resident = 1;
+    
+    if(currentProc.vPages[index]->zero == 1) {
+      memset((char*) pm_physmem + ppage * VM_PAGESIZE, 0, VM_PAGESIZE);
     }
     else {
-      //free space in physical mem
-      int ppage = physicalMem.top();
-      physicalMem.pop();
-    
-      memset((char*) pm_physmem + ppage * VM_PAGESIZE, 0, VM_PAGESIZE);
-
-      currentProc.pageTable.ptes[index].ppage = ppage;
-      currentProc.vPages[index]->resident = 1;
-      currentProc.vPages[index]->zero = 0;
-      tuple<int,vpage_t*> temp = make_tuple(ppage, currentProc.vPages[index]);
-      currentProc.clock.push(temp);
-	//insert((currentProc.clock.end()), currentProc.vPages[index]);
+      disk_read(currentProc.vPages[index]->disk_block, currentProc.pageTable.ptes[index].ppage);
+      //      disk_read(unsigned int block, unsigned int ppage);
     }
+
+    //    currentProc.vPages[index]->zero = 0;
+    tuple<int,vpage_t*> temp = make_tuple(index, currentProc.vPages[index]);
+    currentProc.clock.push(temp);
   }
 
 
   //if access is read
   if (!write_flag){
-
     if (page_table_entry.read_enable == 0){
-      
       currentProc.pageTable.ptes[index].read_enable = 1;
       currentProc.vPages[index]->read = 1;
       currentProc.vPages[index]->reference = 1;
@@ -156,28 +151,19 @@ extern int vm_fault(void *addr, bool write_flag){
 
   //if access is write 
   if (write_flag){
-
-    if ((page_table_entry.read_enable == 0) | (page_table_entry.write_enable == 0)){
-      // do stuff
-
-
+    if ((page_table_entry.read_enable == 0) || (page_table_entry.write_enable == 0)){
       currentProc.pageTable.ptes[index].read_enable = 1;
       currentProc.pageTable.ptes[index].write_enable = 1;
       currentProc.vPages[index]->read = 1;
       currentProc.vPages[index]->write = 1;
       currentProc.vPages[index]->dirty = 1;
       currentProc.vPages[index]->reference = 1;
- 
+      // set zero to 0
       return 0; 
-      
     }
   }
 
-
-
   return -1;
-
-
 }
 
 extern void vm_switch(pid_t pid){
@@ -187,23 +173,35 @@ extern void vm_switch(pid_t pid){
 
 }
 
-extern void vm_destroy(){ return;}
-/**
 extern void vm_destroy(){
-  for(int i = 0; i < currentProc->vPages.size(); i++) {
-    vpage_t* temp = currentProc->vPages.at(i);
-    if(temp.resident) {
-      //add physical mem back, doesnt matter what integer 
-      physicalMem.push(temp.disk_block);
+  for(int i = 0; i < currentProc.vPages.size(); i++) {
+    vpage_t* temp = currentProc.vPages.at(i);
+    if(temp->resident) {
+      physicalMem.push(currentProc.pageTable.ptes[i].ppage);
     }
-    disk.push(temp.disk_block);
+    disk.push(temp->disk_block);
+    free(currentProc.vPages[i]);
+  }
 
-    // free or delete vpage 
+  //Check which process each thing the clock belongs to
+  while(!currentProc.clock.empty()) {
+    currentProc.clock.pop();
   }
 
 
   return;
-  }*/
+}
 
 
-extern int vm_syslog(void *message, unsigned int len){return 1;}
+extern int vm_syslog(void *message, unsigned int len){
+  /** if(len == 0) { //Or invalid address
+    return -1;
+  }
+  string s = "";
+  for(unsigned int i = 0; i < len; i++) {
+    s.append((string) message);
+    message += i;
+  }
+  cout << "syslog \t\t\t" << s << endl;*/
+  return 0;
+}
